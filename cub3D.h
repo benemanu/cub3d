d103e6030007cb8d8031ed53ad7bb59898f49d3d
Benedikt Hielscher <bhielsch@c3r4p5.42vienna.com>
2023-02-28 17:17:00 +0100

    raycast.c & raycast_utils.c

--- cub3D.h
+++ cub3D.h
@@ -11,7 +11,37 @@
 
 #define FILE_ENDING 0
 #define CANNOT_OPEN 1
+#define height 480
+#define width 640
 
+typedef struct s_img
+{
+	void	*img;
+	int		*data;
+	int		img_width;
+	int		img_height;
+	int 	endian;
+	int 	bpp;
+	int 	line_len;
+}				t_img;
+
+typedef struct s_info
+{
+	double posx;
+	double posy;
+	double dirx;
+	double diry;
+	double planex;
+	double planey;
+	void	*mlx;
+	void	*win;
+	t_img 	img;
+	int 	buff[height][width];
+	double	mov_speed;
+	double	rot_speed;
+	int 	**texture;
+
+}				t_info;
 
 typedef struct s_map
 {
