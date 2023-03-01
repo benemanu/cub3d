f68873e20fe7504810c7325edb445cd3aee95915
Benedikt Hielscher <bhielsch@c2r1p9.42vienna.com>
2023-03-01 18:01:35 +0100

    raycast

--- cub3D.h
+++ cub3D.h
@@ -8,63 +8,22 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <math.h>
+# include "raycast.h"
 
 #define FILE_ENDING 0
 #define CANNOT_OPEN 1
 #define height 480
 #define width 640
-#define ALLOCATION	99
-
-typedef struct s_img
-{
-	void	*img;
-	int		*data;
-	int		img_width;
-	int		img_height;
-	int 	endian;
-	int 	bpp;
-	int 	line_len;
-}				t_img;
-
-typedef struct s_info
-{
-	double posx;
-	double posy;
-	double dirx;
-	double diry;
-	double planex;
-	double planey;
-	void	*mlx;
-	void	*win;
-	t_img 	img;
-	int 	buff[height][width];
-	double	mov_speed;
-	double	rot_speed;
-	int 	**texture;
-
-}				t_info;
 
 typedef struct s_map
 {
-	char			**grid;
-	unsigned int	width;
-	unsigned int	height;
-	char			*north_texture;
-	char			*south_texture;
-	char			*west_texture;
-	char			*east_texture;
-	char			*floor_color;
-	char			*ceiling_color;
-}	t_map;
+	char	**grid;
+}			t_map;
 
 //error/exit/free
 void throwError(int code);
 
 //map
 void checkFile(t_map *map, char *filename);
-
-//map_utils
-void printGrid(char **grid);
-int checkIfEmpty(char *str);
 
 #endif
