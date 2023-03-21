24e538f0ab04a339bb213fa08cd903f7ab3f476b
Benedikt Hielscher <bhielsch@c2r1p9.42vienna.com>
2023-03-21 10:23:49 +0100

    Norm

--- src_bonus/crosshair.c
+++ src_bonus/crosshair.c
@@ -1,30 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   crosshair.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bhielsch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/21 10:20:41 by bhielsch          #+#    #+#             */
+/*   Updated: 2023/03/21 10:21:23 by bhielsch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../inc_bonus/raycast_bonus.h"
 
-void    ft_print_crosshair(t_info *info)
+void	ft_print_crosshair(t_info *info)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    i = - 11;
-    while (i < 11)
-    {
-        j = -2;
-        while (j < 0)
-        {
-            ft_my_mlx_pixel_put(info, WIDTH / 2 + i, HEIGHT / 2 + j, 0xFF0000);
-            j++;
-        }
-        i++;
-    }
-    i = -2;
-    while (i < 0)
-    {
-        j = -11;
-        while (j < 11)
-        {
-            ft_my_mlx_pixel_put(info, WIDTH / 2 + 1 + i, HEIGHT / 2 + j, 0xFF0000);
-            j++;
-        }
-        i++;
-    }
+	i = -12;
+	while (++i < 11)
+	{
+		j = -3;
+		while (++j < 0)
+			ft_my_mlx_pixel_put(info, WIDTH / 2 + i, HEIGHT / 2 + j, 0xFF0000);
+	}
+	i = -3;
+	while (++i < 0)
+	{
+		j = -12;
+		while (++j < 11)
+			ft_my_mlx_pixel_put(info, WIDTH / 2 + 1 + i, HEIGHT / 2 + j,
+				0xFF0000);
+	}
 }
